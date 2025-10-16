33cf91d25a04620b1788090aa760a77a4f1c0dc1
Lucas de Assis Mudo <lucas.assis.mudo@gmail.com>
2025-10-16 12:04:24 -0300

    fix(backend) - added @CrossOrigin annotation to Employee controller class

--- backend/src/main/java/com/lucasassismudo/task_manager_springboot/controller/employee/EmployeeController.java
+++ backend/src/main/java/com/lucasassismudo/task_manager_springboot/controller/employee/EmployeeController.java
@@ -4,16 +4,14 @@ import com.lucasassismudo.task_manager_springboot.dto.TaskDto;
 import com.lucasassismudo.task_manager_springboot.services.employee.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/employee")
+@CrossOrigin("*")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
