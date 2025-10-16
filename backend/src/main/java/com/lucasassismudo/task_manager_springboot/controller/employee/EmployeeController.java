ea56b4e5ad0c7ad37abc119a178ece8ac4125660
Lucas de Assis Mudo <lucas.assis.mudo@gmail.com>
2025-10-16 15:26:39 -0300

    feature(backend) - added update task status API to employees

--- backend/src/main/java/com/lucasassismudo/task_manager_springboot/controller/employee/EmployeeController.java
+++ backend/src/main/java/com/lucasassismudo/task_manager_springboot/controller/employee/EmployeeController.java
@@ -3,6 +3,7 @@ package com.lucasassismudo.task_manager_springboot.controller.employee;
 import com.lucasassismudo.task_manager_springboot.dto.TaskDto;
 import com.lucasassismudo.task_manager_springboot.services.employee.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,5 +20,14 @@ public class EmployeeController {
     @GetMapping("/tasks")
     public ResponseEntity<List<TaskDto>> getTasksByUserId(){
         return ResponseEntity.ok(employeeService.getTasksByUserId());
+    }
+
+    @PutMapping("/task/{id}/{status}")
+    public ResponseEntity<TaskDto> updateTaskStatus(@PathVariable Long id, @PathVariable String status){
+        TaskDto updatedTask = employeeService.updateTaskStatus(id, status);
+        if (updatedTask == null){
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(updatedTask);
     }
 }
--- backend/src/main/java/com/lucasassismudo/task_manager_springboot/services/employee/EmployeeServieImpl.java
+++ backend/src/main/java/com/lucasassismudo/task_manager_springboot/services/employee/EmployeeServieImpl.java
@@ -3,6 +3,7 @@ package com.lucasassismudo.task_manager_springboot.services.employee;
 import com.lucasassismudo.task_manager_springboot.dto.TaskDto;
 import com.lucasassismudo.task_manager_springboot.entities.Task;
 import com.lucasassismudo.task_manager_springboot.entities.User;
+import com.lucasassismudo.task_manager_springboot.enums.TaskStatus;
 import com.lucasassismudo.task_manager_springboot.repositories.TaskRepository;
 import com.lucasassismudo.task_manager_springboot.utils.JwtUtil;
 import jakarta.persistence.EntityNotFoundException;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,5 +35,26 @@ public class EmployeeServieImpl implements EmployeeService{
                     .collect(Collectors.toList());
         }
         throw new EntityNotFoundException("User not found!");
+    }
+
+    @Override
+    public TaskDto updateTaskStatus(Long id, String status) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()){
+            Task existingTask = optionalTask.get();
+            existingTask.setTaskStatus(mapStringToTaskStatus(status));
+            return taskRepository.save(existingTask).getTaskDto();
+        }
+        throw new EntityNotFoundException("Task not found!");
+    }
+
+    private TaskStatus mapStringToTaskStatus(String status){
+        return switch (status){
+            case "PENDING" -> TaskStatus.PENDING;
+            case "IN_PROGRESS" -> TaskStatus.IN_PROGRESS;
+            case "COMPLETED" -> TaskStatus.COMPLETED;
+            case "DEFERRED" -> TaskStatus.DEFERRED;
+            default -> TaskStatus.CANCELLED;
+        };
     }
 }
